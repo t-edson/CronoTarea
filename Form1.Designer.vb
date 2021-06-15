ccaeb0c1aacb382a77b3883edd85d0fe290e1996
Tito Hinostroza <tehh2@DESKTOP-IQ840EC>
2021-06-15 12:21:27 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.btnAgregBot = New System.Windows.Forms.ToolStripLabel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(36, 36)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnAgregBot})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(800, 39)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'btnAgregBot
+        '
+        Me.btnAgregBot.Image = CType(resources.GetObject("btnAgregBot.Image"), System.Drawing.Image)
+        Me.btnAgregBot.Name = "btnAgregBot"
+        Me.btnAgregBot.Size = New System.Drawing.Size(138, 36)
+        Me.btnAgregBot.Text = "&Agregar Tarea"
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 39)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(800, 411)
+        Me.FlowLayoutPanel1.TabIndex = 1
+        Me.FlowLayoutPanel1.WrapContents = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "Form1"
+        Me.Text = "CronoTarea"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents btnAgregBot As ToolStripLabel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private enfocado As Boolean
+
+    Private Sub btnAgregBot_MouseEnter(sender As Object, e As EventArgs) Handles btnAgregBot.MouseEnter
+        enfocado = True
+        btnAgregBot.Invalidate()
+    End Sub
+    Private Sub btnAgregBot_MouseLeave(sender As Object, e As EventArgs) Handles btnAgregBot.MouseLeave
+        enfocado = False
+        btnAgregBot.Invalidate()
+    End Sub
+    Private Sub btnAgregBot_Paint(sender As Object, e As PaintEventArgs) Handles btnAgregBot.Paint
+        'MyBase.OnPaint(e)   'Evento de la clase base
+        Dim myPen As System.Drawing.Pen = New System.Drawing.Pen(Color.LightSkyBlue, 2)
+        If enfocado Then
+            e.Graphics.DrawRectangle(myPen,
+                New Rectangle(New Drawing.Point(1, 1), New Drawing.Size(btnAgregBot.Width - 2, btnAgregBot.Height - 2))
+            )
+        End If
+    End Sub
+    Private Sub DetenerTodos()
+        'Detiene todos los contadores
+        For Each c As Control In FlowLayoutPanel1.Controls
+            If TypeOf c Is PKUserControl.UserControl1 Then
+                Dim cont As PKUserControl.UserControl1 = DirectCast(c, PKUserControl.UserControl1)
+                cont.Pausar()
+            End If
+        Next
+    End Sub
+    Private Sub btnAgregBot_Click(sender As Object, e As EventArgs) Handles btnAgregBot.Click
+        Dim txt As String
+        txt = InputBox("Nombre: ")
+        Dim cont As New PKUserControl.UserControl1(txt)
+        FlowLayoutPanel1.Controls.Add(cont)
+        'Agrega eventos
+        AddHandler cont.OnInicio,
+            Sub()
+                DetenerTodos()
+                cont.Iniciar()
+            End Sub
+    End Sub
+
+End Class
